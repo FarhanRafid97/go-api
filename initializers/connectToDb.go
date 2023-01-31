41d87d53d56fcbb97b26da57fe8f4ec88d7b2489
1371101711970002 <farhanrafid97@gmail.com>
2023-01-31 16:24:59 +0700

    initial init learn golang rest api

--- initializers/connectToDb.go
+++ initializers/connectToDb.go
@@ -2,6 +2,7 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -11,7 +12,8 @@ var DB *gorm.DB
 
 func ConnectToDb() {
 	var err error
-	dsn := "host=localhost user=farhan_binar password=farhan_322 dbname=go_api port=5432  sslmode=disable"
+	URL_DB := os.Getenv("ULR_DB")
+	dsn := URL_DB
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed connect to db")
